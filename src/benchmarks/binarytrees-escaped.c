26c2e684cbaab503fcbdaa546a36b738bb0221d3
Olle Harstedt <olleharstedt@yahoo.com>
2021-01-26 23:33:34 +0100

    Add example that won't compile (missing lib, but which?)

--- src/benchmarks/binarytrees-escaped.c
+++ src/benchmarks/binarytrees-escaped.c
@@ -1,3 +1,17 @@
 /**
  * What escape-c should compile down to.
+ *
+ * Compile with:
+ *   gcc -L/usr/lib/ocaml -lcamlrun -lm -ldl src/benchmarks/binarytrees-escaped.c
+ *
+ * @see https://caml.inria.fr/pub/docs/manual-ocaml/intfc.html
  */
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+
+void foo(value v1, value v2, value v3)
+{
+    CAMLparam3 (v1, v2, v3);
+    CAMLreturn0;
+}
